b725cea4dc292b052b232b9c1f81ab3d45c21cfe
ZeroRequiem126 <noboruonishi0126@gmail.com>
2022-04-06 21:10:21 +0900

    文字コード指定

--- assets/templates/library.php
+++ assets/templates/library.php
@@ -1,6 +1,6 @@
 <?php
     function dbconnect() {
-        $dsn = 'mysql:dbname=heroku_40d84b790f4e885;host=us-cdbr-east-05.cleardb.net';
+        $dsn = 'mysql:dbname=heroku_40d84b790f4e885;host=us-cdbr-east-05.cleardb.net;charset=utf8mb4';
         $user = 'b2e469a773bc99';
         $password = '2c381f19';
 
