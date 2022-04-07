6c618068ce1a9cfc43e35bb13dd833bdd296f8f9
ZeroRequiem126 <noboruonishi0126@gmail.com>
2022-04-07 22:20:36 +0900

    データベース接続情報を修正

--- assets/templates/library.php
+++ assets/templates/library.php
@@ -1,6 +1,6 @@
 <?php
     function dbconnect() {
-        $dsn = 'mysql:dbname=heroku_ed09d36c9723ee2;host=us-cdbr-east-05.cleardb.net';
+        $dsn = 'mysql:dbname=heroku_ed09d36c9723ee2;host=us-cdbr-east-05.cleardb.net;charset=utf8mb4';
         $user = 'bd34269043211f';
         $password = '6120bfde';
 
