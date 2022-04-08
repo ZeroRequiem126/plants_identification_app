878217b787fc9e2c5594808fa6ba69574da4312d
ZeroRequiem126 <noboruonishi0126@gmail.com>
2022-04-08 17:30:44 +0900

    composer.jsonを作成

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4e953edb8297d2cc9a692a400799c57190cf5c31',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4e953edb8297d2cc9a692a400799c57190cf5c31',
+            'dev_requirement' => false,
+        ),
+    ),
+);
