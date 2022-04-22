8f636f005fcad0e6a5c58268d8548e6ad6b73381
ZeroRequiem126 <noboruonishi0126@gmail.com>
2022-04-22 21:42:56 +0900

    Test Commit

--- assets/templates/library.php
+++ assets/templates/library.php
@@ -1,12 +1,12 @@
 <?php
     function dbconnect() {
-        $dsn = 'mysql:dbname=heroku_ed09d36c9723ee2;host=us-cdbr-east-05.cleardb.net;charset=utf8mb4';
-        $user = 'bd34269043211f';
-        $password = '6120bfde';
+        $dsn = 'mysql:dbname=218vy_mydb;host=mysql90.conoha.ne.jp;charset=utf8mb4';
+        $user = '218vy_noboru';
+        $password = 'tsu9689da2!';
         
         try{
             $dbh = new PDO($dsn, $user, $password, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,`
             ]);
             return $dbh;
         } catch(PDOException $e){
